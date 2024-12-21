7e4cccf60c21cf43bf539a89e708eda39a60eee1
EliasPires <elias.cpires@gmail.com>
2024-12-20 22:23:39 -0300

    Add new classes

--- src/main/java/com/monterrey/dscommerce/entities/User.java
+++ src/main/java/com/monterrey/dscommerce/entities/User.java
@@ -14,7 +14,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(unique = true)
     private String email;
+
     private String fone;
     private LocalDate birthDate;
     private String password;
